1843ac766f535d5455868832fca89237a34b855a
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2024-02-10 13:47:18 -0500

    added portfolio section

---
+++ src/pages/Portfolio/project.js
@@ -0,0 +1,8 @@
+export const projects = [
+    {
+      title: "Marvel Super Hero Personality Quiz",
+      description: "A personality quiz that matches you with a Marvel Super Hero based on your answers. **As of the making of this portfolio this site is not live. Linked below is the github repository.**",
+      image: "src/pages/Portfolio/MarvelGroupProject1.PNG",
+      url: "https://github.com/MArrasmith/Marvel-Heroes-Personality-Quiz",
+}
+]
