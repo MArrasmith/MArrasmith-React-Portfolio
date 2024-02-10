45ca508e0425653e791316c5495399220947a6f3
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2024-02-10 15:59:04 -0500

    added contact and resume  files and csss

--- src/pages/Portfolio/project.js
+++ src/pages/Portfolio/project.js
@@ -1,8 +1,8 @@
 export const projects = [
     {
       title: "Marvel Super Hero Personality Quiz",
-      description: "A personality quiz that matches you with a Marvel Super Hero based on your answers. **As of the making of this portfolio this site is not live. Linked below is the github repository.**",
+      description: "A personality quiz that matches you with a Marvel Super Hero based on your answers.",
       image: "src/pages/Portfolio/MarvelGroupProject1.PNG",
-      url: "https://github.com/MArrasmith/Marvel-Heroes-Personality-Quiz",
+      url: "https://marrasmith.github.io/Marvel-Heroes-Personality-Quiz/",
 }
 ]
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Resume.css';
+import resumeLink from './resumeImages/resumeLink.PNG';
+import resumeLink2 from './resumeImages/resumeLink.PNG'
 
 function Resume() {
   return (
-    <div id="resume">
+    <div id="resume" class="resume-container">
       <h1>My resume</h1>
-
+      <p>
+        <a href="https://docs.google.com/document/d/e/2PACX-1vRT4-GlfiW23MBrSwRMTku18NxSWsppNZuGvjeEM87Npl1bf3_t3oQ5sRNAkmbETw/pub" target="_blank">My resumé</a>
+      </p>
+      <div>
+        <img src={resumeLink} alt="resume" />
+        <img src={resumeLink2} alt="resume2" />
+      </div>
     </div>
   )
 }
