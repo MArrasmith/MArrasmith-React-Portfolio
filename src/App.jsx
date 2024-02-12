2458f8e621f988ba542e5c17ca5a61be611db345
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2024-02-12 11:38:56 -0500

    deleted index.js and moved everything into app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.css';
-import { Navbar, About, Portfolio, Resume, Contact, Technologies } from './pages';
+import Navbar from './pages/Navbar/Navbar';
+import About from './pages/About/About';
+import Technologies from './pages/Technologies/Technologies';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+import Contact from './pages/Contact/Contact';
+
 //imports the pages for the website
 function App() {
   return (
