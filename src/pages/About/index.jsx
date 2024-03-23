380a22c8d3fb870e69907b485425df76a358885f
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2024-03-23 13:51:31 -0400

    edited about me

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -8,7 +8,7 @@ function About() {
       <div>
         <h1>About Me</h1>
         <p>
-          Michael is a full stack web developer with a passion for building websites and applications with a clean and logical user interface.  He is a quick learner, a team player, and a strong communicator who enjoys working with a team to solve complex problems. Michael has a background rich with artistic influence. Growing up he was always heavily involved in local theater and dance, studying ballet well into adulthood. He also loves to draw and paint, some of his works being featured on an international scale. Michael is a student enrolled in the Full Stack Web Development Bootcamp at The Ohio State University in partnership with EdX, and is excited to bring his unique perspective to the world of web development.
+        Michael is a full stack web developer with a passion for building websites and applications with a clean and logical user interface. He is a quick learner, a team player, and a strong communicator who enjoys working with a team to solve complex problems. Michael has a background rich with artistic influence. Growing up he was always heavily involved in local theater and dance, studying ballet well into adulthood. He also loves to draw and paint, some of his works being featured on an international scale. Michael successfully completed the Full Stack Web Development Bootcamp at The Ohio State University in partnership with EdX, and is excited to bring his unique perspective to the world of web development.
         </p>
       </div>
     </div>
