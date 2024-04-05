a32e607bdda81d8d3c98c0bb1453c277f3fdfef5
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2024-04-05 00:39:32 -0400

    edited resume

--- src/pages/Portfolio/project.js
+++ src/pages/Portfolio/project.js
@@ -2,7 +2,7 @@ export const projects = [
     {
       title: "Marvel Super Hero Personality Quiz",
       description: "A personality quiz that matches you with a Marvel Super Hero based on your answers.",
-      image: "src/pages/Portfolio/MarvelGroupProject1.PNG",
+      // image: "src/pages/Portfolio/portfolio-images/MarvelGroupProject1.PNG",
       url: "https://marrasmith.github.io/Marvel-Heroes-Personality-Quiz/",
 }
 ]
--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -8,8 +8,12 @@ function Resume() {
       <h1>My Resumé</h1>
       <p>
         <a href="https://drive.google.com/file/d/1r0kTJfCtQYkYkun7_XbyqIGaxZgfca5R/view?usp=sharing" target="_blank" rel="noreferrer">
-          <img class="resume"src="src/pages/Resume/public/images/techresumescreenshot.PNG"></img>
+          <img class="resume1"src="src/pages/Resume/public/images/techresume1.PNG"></img>
         </a>
+        <a href="https://drive.google.com/file/d/1r0kTJfCtQYkYkun7_XbyqIGaxZgfca5R/view?usp=sharing" target="_blank" rel="noreferrer">
+          <img class="resume2"src="src/pages/Resume/public/images/techresume2.PNG"></img>
+        </a>
+        <h3>Click me! ☝️ </h3>
       </p>
     </div>
   )
